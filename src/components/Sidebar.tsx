f69f9548bc25f0b7d68ad9187cd7892f60b5063f
Manuel Velado Jr <manuel.veladojr@gmail.com>
2025-02-24 04:36:05 +0800

    Update files with latest changes

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,16 @@
+// src/components/Sidebar.tsx
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="w-64 bg-gray-200 p-4">
+      <ul>
+        <li>Option 1</li>
+        <li>Option 2</li>
+        <li>Option 3</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
