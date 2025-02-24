3198486c2125fd313f9e55ff7d84102b1a2a27dd
Manuel Velado Jr <manuel.veladojr@gmail.com>
2025-02-24 11:53:07 +0800

    Update files with latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Root is now the project root
-  root: './',
-  // Specify public directory for static assets
-  publicDir: 'public',
+  root: 'public',
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: '../dist'
   }
 })
