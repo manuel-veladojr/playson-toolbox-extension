d5dd900e083538fa062ff934c7272e2bc31f42b1
Manuel Velado Jr <manuel.veladojr@gmail.com>
2025-02-24 12:02:20 +0800

    Update files with latest changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'public',
   plugins: [react()],
-  build: {
-    outDir: '../dist'
-  }
-})
+  // Ensure publicDir is correctly set (by default it's "public")
+  publicDir: 'public'
+});
