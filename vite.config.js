a4508f33757e5669f04279f5412901901a27ccf9
Manuel Velado Jr <manuel.veladojr@gmail.com>
2025-02-24 12:08:15 +0800

    Update files with latest changes

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "public", // Set the root to the public folder
+  publicDir: "public", // This is the folder that contains static assets (manifest.json, index.html, etc.)
   build: {
-    outDir: "../dist", // Build output folder
+    outDir: "dist", // Build output folder
     emptyOutDir: true,
   },
 });
